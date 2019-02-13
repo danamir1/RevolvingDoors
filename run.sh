323b99324fac77d1567db855274c11cfc69ee5cd
GalPatel <04011993gal!@#>
2019-02-14 01:55:02 +0200

    more time

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem=6000m
 #SBATCH -c4
-#SBATCH --time=1-0
+#SBATCH --time=7-0
 #SBATCH --gres=gpu:0
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=gal.patel@mail.huji.ac.il
