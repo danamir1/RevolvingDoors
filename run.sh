38c6817c79cd68185a0263535dc27c360d84d64b
GalPatel <gal.patel@mail.huji.ac.il>
2019-02-14 01:53:41 +0200

    bash

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --mem=6000m
+#SBATCH -c4
+#SBATCH --time=1-0
+#SBATCH --gres=gpu:0
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=gal.patel@mail.huji.ac.il
+
+dir=/cs/labs/oabend/gal.patel/tasks/RevolvingDoors/
+
+cd $dir
+
+source /cs/labs/oabend/gal.patel/virtualenvs/haystack-venv/bin/activate
+python entities_organization.py
