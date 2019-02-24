2b2fc57afae2f98be7fa7310e8a0e5f643d8924b
GalPatel <04011993gal!@#>
2019-02-24 09:05:36 +0200

    filter

--- cluster_visualization.py
+++ cluster_visualization.py
@@ -17,15 +17,54 @@ import json
 
 ENTITIES_DATABASE = 'data/entities/entities_database.csv'
 MAYA_MATCH = 'data/ynet/maya_name_matches.csv'
-TYPE_MAP = 'data/types_mapping.json'
+TYPE_MAP = 'types_mapping.json'
+
 
 def load_pickle(path):
     with open(path, "rb") as f:
         return pickle.load(f)
 
+
 def load_json(path):
-    with open(path, 'rb') as f:
+    with open(path, encoding='utf-8') as f:
+        # f = f.read().encode('utf-8')
         return json.load(f)
+
+
+def get_type_mapping():
+    return {
+        "חברה פרטית": "company",
+        "חברה ציבורית": "company",
+        "חברה פרטית מחוייבת במאזן": "company",
+        "חברת חו\"ל": "company",
+                             "שותפות מוגבלת": "company",
+        "NaN": "other",
+        "government_office": "government",
+        "municipality": "government",
+        "religion_service": "government",
+        "foreign_company": "company",
+        "law_mandated_organization": "government",
+        "provident_fund": "company",
+        "university": "company",
+        "health_service": "company",
+        "professional_association": "company",
+        "house_committee": "company",
+        "foreign_representative": "other",
+        "municipal_parties": "other",
+        "conurbation": "government",
+        "local_planning_committee": "government",
+        "local_community_committee": "government",
+        "religious_court_sacred_property": "government",
+        "drainage_authority": "government",
+        "municipal_precinct": "other",
+        "west_bank_corporation": "company",
+        "company": "company",
+        "association": "company",
+        "cooperative": "company",
+        "ottoman-association": "company",
+        "private": "company"
+    }
+
 
 
 def apriori_cluster(baskets_file='data/ynet/articles_orgs_baskets_190222.pkl'):
@@ -35,34 +74,42 @@ def apriori_cluster(baskets_file='data/ynet/articles_orgs_baskets_190222.pkl'):
     result = apriori(baskets, min_support=0.002, min_confidence=0.0, min_lift=0.002,
                      max_length=None)
     for i, r in enumerate(result):
-        print(i, r.items)#, r.support)
+        print(i, r.items)  # , r.support)
+
 
 def edit_distance(s1, s2):
     # from https://stackoverflow.com/questions/2460177/edit-distance-in-python
-    m=len(s1)+1
-    n=len(s2)+1
+    m = len(s1) + 1
+    n = len(s2) + 1
 
     tbl = {}
-    for i in range(m): tbl[i,0]=i
-    for j in range(n): tbl[0,j]=j
+    for i in range(m): tbl[i, 0] = i
+    for j in range(n): tbl[0, j] = j
     for i in range(1, m):
         for j in range(1, n):
-            cost = 0 if s1[i-1] == s2[j-1] else 1
-            tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
+            cost = 0 if s1[i - 1] == s2[j - 1] else 1
+            tbl[i, j] = min(tbl[i, j - 1] + 1, tbl[i - 1, j] + 1, tbl[i - 1, j - 1] + cost)
 
-    return tbl[i,j]
+    return tbl[i, j]
 
-def match_entities(filtered_orgs):
+
+def match_entities(orgs):
     # interactive matching of entities to the entities dataset. TODO: use merge sets and maya for better matching
-    new_orgs = {'org_name':[],'entity_name':[],'id':[], 'type':[], 'sub_type':[],'candidates':[]}
+    new_orgs = {'org_name': [], 'entity_name': [], 'id': [], 'type': [], 'sub_type': [],
+                'candidates': []}
     maya = pd.read_csv(MAYA_MATCH)
     entities_dataset = pd.read_csv(ENTITIES_DATABASE)
-    type_map = load_json(TYPE_MAP)
-    for org in tqdm.tqdm(filtered_orgs):
-        candidates = entities_dataset[entities_dataset['name'].str.contains(org,regex=False)]
+    # print(entities_dataset.columns.values)
+    type_map = get_type_mapping()
+    for i, org in enumerate(orgs):
+        print('-->', org)
+        # candidates = entities_dataset[entities_dataset['name'].str.contains(org, regex=False)]
+        candidates = entities_dataset[org in entities_dataset.name]
         if ldetect(org) != 'he':
-            en_candidates = entities_dataset[entities_dataset['eng_name'].str.contains(org,regex=False)]
-            candidates = pd.concat([candidates, en_candidates]).drop_duplicates().reset_index(drop=True)
+            en_candidates = entities_dataset[
+                entities_dataset['eng_name'].str.contains(org, regex=False)]
+            candidates = pd.concat([candidates, en_candidates]).drop_duplicates().reset_index(
+                drop=True)
         if candidates.size == 0:
             # no candidate
             new_orgs['org_name'].append(org)
@@ -75,7 +122,9 @@ def match_entities(filtered_orgs):
             # look for the one with the smallest edit distance
             min_dis = float('inf')
             best_candidate = None
+            print('-->', org)
             for index, row in candidates.iterrows():
+                print(row['name'])
                 if ldetect(org) != 'he':
                     distance = edit_distance(org, row['name_eng'])
                 else:
@@ -89,13 +138,12 @@ def match_entities(filtered_orgs):
             new_orgs['candidates'].append(candidates.size)
             new_orgs['sub_type'].append(best_candidate['type'])
             new_orgs['type'].append(type_map[best_candidate['type']])
-
-
-
+        if i > 20:
+            break
 
     # pd.DataFrame(new_orgs).to_csv('data/ynet/filtered_by_entity_db.csv', encoding='utf8')
     # pd.DataFrame({'org_name': unmatched_orgs}).to_csv("data/ynet/unmatched_orgs.csv",encoding='utf8')
-    pd.DataFrame(new_orgs).to_csv('data/ynet/filtered_by_entity_db.csv', encoding='utf8')
+    pd.DataFrame(new_orgs).to_csv('data/ynet/matched.csv', encoding='utf8')
     # pd.DataFrame({'org_name': unmatched_orgs}).to_csv("data/ynet/unmatched_orgs.csv", encoding='utf8')
     return new_orgs
 
@@ -116,7 +164,10 @@ def visualize(idx2org, org2idx, org2pairs, distance, affinity, distance_threshol
     graph.opts(node_size=2, edge_line_width=hv.dim('weight'))
     hv.save(graph, "out.html")
 
+
 if __name__ == '__main__':
     # apriori_cluster()
-
-    match_entities()
+    # todo try: http://holoviews.org/gallery/demos/bokeh/lesmis_example.html
+    # todo search: https://bokeh.pydata.org/en/latest/docs/user_guide/interaction/widgets.html
+    orgs = list(load_pickle('data/ynet/orgs_to_articles_190222.pkl').keys())
+    match_entities(orgs)
